09e7ce31c88cc88c0b0383323ba2150d2b01ca12
Szentgyörgyi Péter <peti0202@windowslive.com>
2025-08-01 07:44:45 +0200

    Admin oldal készítése

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,9 +7,29 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg section">
                 <div class="p-6 text-gray-900">
-                    {{ __("Sikeres bejelentkezés!") }}
+                    <h1 class="section-title">Bevezető szöveg</h1>
+                    <form action="" method="post">
+                        @csrf
+                        @method('PUT')
+
+                        <div class="input-field-wrapper">
+                            <label class="title" for="title">Cím</label>
+                            <input class="input" type="text" name="title" id="title">
+                        </div>
+                        
+                        <div class="input-field-wrapper">
+                            <label class="subtitle" for="subtitle">Alcím</label>
+                            <input class="input" type="text" name="subtitle" id="subtitle">
+                        </div>
+
+                        <div class="input-field-wrapper">
+                            <label class="text" for="text">Szöveg</label>
+                            <textarea class="text-input" name="text" id="text"></textarea>
+                        </div>
+                        <button class="btn" type="submit">Frissítés</button>
+                    </form>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,6 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
 Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/dashboard', function () {
@@ -17,6 +14,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    
 });
 
 require __DIR__.'/auth.php';
